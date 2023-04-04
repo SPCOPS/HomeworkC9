95994725fbabda8b3d0fac131d07157a2acd4e72
SPECTR <ivanov8465@mail.ru>
2023-04-04 21:18:52 +0300

    Сделал 3 задание

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int Akkerman(int m, int n)
+{
+   if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+Console.Clear();
+Console.Write("Введите положительное число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите положительное число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"A({m}, {n}) -> {Akkerman(m, n)}");
