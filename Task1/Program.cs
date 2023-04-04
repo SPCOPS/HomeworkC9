99b15379d15d659499e59d6ece5d03dd211bc81b
SPECTR <ivanov8465@mail.ru>
2023-04-04 21:18:33 +0300

    Сделал 1 задание

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int Num(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{Num(n, m + 1)}, ");
+    return m;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(Num(n, m));
